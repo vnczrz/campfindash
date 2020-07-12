92924722fecc6afa2f1c1d1e73c22052919c1fbf
vnczrz <1.800.vince@gmail.com>
2020-07-11 19:28:11 -0700

    try again

--- app/settings.py
+++ app/settings.py
@@ -8,5 +8,4 @@ from os import environ
 SECRET_KEY = environ.get('SECRET_KEY')
 API_KEY = environ.get('API_KEY')
 
-##where to find the database and initialize SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
