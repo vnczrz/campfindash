c789b1db1132d8603ce07508885b8dbfd6d2a7df
vnczrz <1.800.vince@gmail.com>
2020-07-11 19:00:29 -0700

    env postgre

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,7 @@
 import os
 from os import environ 
 
+DATABASE_URL = 'postgresql://postgres:Barkley.915@127.0.0.1/postgres'
 # SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///congress.sqlite3')
 SECRET_KEY = environ.get('SECRET_KEY')
 API_KEY = environ.get('API_KEY')
