4f13e337e30f5f2f12b89f1b4c0f7ebcf9aa8d40
vnczrz <1.800.vince@gmail.com>
2020-07-12 13:38:03 -0700

    herokudb

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def after_request(response):
 
 
 ##where to find the database and initialize SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(DATABASE_URL)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) 
 
